08f012d785ae941291e0e893b1c88b46e458f801
jmacelroy <30531294+jmacelroy@users.noreply.github.com>
2020-07-24 12:12:36 -0500

    Parsing url from request data and using signal api.

--- src/meet_processor.ts
+++ src/meet_processor.ts
@@ -1,7 +1,8 @@
-import util from 'util';
-import fs from 'fs';
 import * as dotenv from 'dotenv';
-import { RecorderRequestMeta } from './request_tracker';
+import got from 'got';
+import NodeCache from 'node-cache';
+import { sign } from 'jsonwebtoken';
+import { RecorderRequestMeta, Update, RecorderRequest } from './request_tracker';
 import { JibriTracker } from './jibri_tracker';
 import { recorderToken } from './token';
 import logger from './logger';
@@ -16,7 +17,7 @@ export const AsapJwtKid: string = process.env.ASAP_JWT_KID;
 export const AsapJwtAcceptedAud: string = process.env.ASAP_JWT_AUD;
 export const AsapJwtAcceptedIss: string = process.env.ASAP_JWT_ACCEPTED_ISS;
 export const AsapJwtAcceptedHookIss: string = process.env.ASAP_JWT_ACCEPTED_HOOK_ISS;
-const TokenSigningKeyFile: string = process.env.TOKEN_SIGNING_KEY_FILE;
+export const TokenSigningKeyFile: string = process.env.TOKEN_SIGNING_KEY_FILE;
 
 const requiredConfig: Array<number | string> = [
     AsapPubKeyBaseUrl,
@@ -34,38 +35,82 @@ requiredConfig.forEach((val, i) => {
     }
 });
 
-let jwtSigningKey: Buffer = undefined;
-const readFile = util.promisify(fs.readFile);
-readFile(TokenSigningKeyFile).then((buff) => {
-    jwtSigningKey = buff;
-});
+export interface MeetProcessorOptions {
+    jibriTracker: JibriTracker;
+    signingKey: Buffer;
+}
+
+interface TokenResponse extends RecorderRequest {
+    token: string;
+}
 
 export class MeetProcessor {
     private jibriTracker: JibriTracker;
+    private signingKey: Buffer;
+    private asapCache: NodeCache;
 
-    constructor(jibriTracker: JibriTracker) {
-        this.jibriTracker = jibriTracker;
+    constructor(options: MeetProcessorOptions) {
+        this.jibriTracker = options.jibriTracker;
+        this.signingKey = options.signingKey;
+        this.asapCache = new NodeCache({ stdTTL: 60 * 45 }); // TTL of 45 minutes
         this.requestProcessor = this.requestProcessor.bind(this);
+        this.updateProcessor = this.updateProcessor.bind(this);
+    }
+
+    authToken(): string {
+        const cachedAuth: string = this.asapCache.get('asap');
+        if (cachedAuth) {
+            return cachedAuth;
+        }
+
+        const auth = sign({}, this.signingKey, {
+            issuer: AsapJwtIss,
+            audience: 'jitsi',
+            algorithm: 'RS256',
+            keyid: AsapJwtKid,
+            expiresIn: 60 * 60, // 1 hour
+        });
+
+        this.asapCache.set('asap', auth);
+        return auth;
     }
 
     async requestProcessor(req: RecorderRequestMeta): Promise<boolean> {
-        try {
-            const jibriId = await this.jibriTracker.nextAvailable();
-            const token = recorderToken(
-                {
-                    issuer: AsapJwtIss,
-                    audience: 'jitsi',
-                    subject: '*',
-                    algorithm: 'RS256',
-                    keyid: AsapJwtKid,
-                    expiresIn: RecorderTokenExpSeconds,
-                },
-                jwtSigningKey,
-            );
-        } catch (err) {
-            logger.error(`unable to process request ${err}`);
-            return false;
+        const jibriId = await this.jibriTracker.nextAvailable();
+        const token = recorderToken(
+            {
+                issuer: AsapJwtIss,
+                audience: 'jitsi',
+                subject: '*',
+                algorithm: 'RS256',
+                keyid: AsapJwtKid,
+                expiresIn: RecorderTokenExpSeconds,
+            },
+            this.signingKey,
+        );
+
+        const recorderResponse: TokenResponse = {
+            conference: req.conference,
+            roomParam: req.roomParam,
+            externalApiUrl: req.externalApiUrl,
+            eventType: 'QueueUpdate',
+            participant: req.participant,
+            requestId: req.requestId,
+            token: token,
+        };
+
+        const response = await got.post(req.externalApiUrl, {
+            searchParams: { room: req.roomParam },
+            headers: {
+                Authorization: `Bearer ${this.authToken()}`,
+            },
+            json: recorderResponse,
+        });
+
+        if (response.statusCode != 200) {
+            throw new Error('non-200 response from token response api');
         }
+
         return true;
     }
 
@@ -75,6 +120,29 @@ export class MeetProcessor {
         const diffTime = Math.trunc(Math.abs((now - created) / 1000));
         if (diffTime >= 2) {
             logger.debug(`request update ${req.requestId} position: ${position} time: ${diffTime}`);
+            const update: Update = {
+                conference: req.conference,
+                roomParam: req.roomParam,
+                externalApiUrl: req.externalApiUrl,
+                eventType: 'QueueUpdate',
+                participant: req.participant,
+                requestId: req.requestId,
+                position: position,
+                time: diffTime,
+            };
+
+            // TODO: metrics, retry
+            const response = await got.post(req.externalApiUrl, {
+                searchParams: { room: req.roomParam },
+                headers: {
+                    Authorization: `Bearer ${this.authToken()}`,
+                },
+                json: update,
+            });
+
+            if (response.statusCode != 200) {
+                throw new Error('non-200 response from token response api');
+            }
         }
         return true;
     }

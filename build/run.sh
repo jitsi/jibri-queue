222affb21134e71c555ac704cbfbb5e1172a317a
jmacelroy <30531294+jmacelroy@users.noreply.github.com>
2020-08-04 14:13:59 -0500

    Moving pre-run script default location.

--- build/run.sh
+++ build/run.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/bin/sh
+
+set -e
 
 # pre-run.sh is intended to include environment specific
 # setup such as env var injection of secrets.
-if [ -f /usr/bin/pre-run.sh ]; then
-    . /usr/bin/pre-run.sh
+if [ -f /usr/jitsi/pre-run.sh ]; then
+    . /usr/jitsi/pre-run.sh
 fi
 
 node /usr/src/app/app.js
